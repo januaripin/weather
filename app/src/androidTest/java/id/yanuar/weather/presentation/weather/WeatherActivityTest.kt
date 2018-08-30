8ed3c1b4dde8166adcc066a419c6b23dd043f702
Yanuar Arifin <halo@yanuar.id>
2018-08-31 00:52:22 +0700

    fix travis and add grant permissions rule

--- app/src/androidTest/java/id/yanuar/weather/presentation/weather/WeatherActivityTest.kt
+++ app/src/androidTest/java/id/yanuar/weather/presentation/weather/WeatherActivityTest.kt
@@ -5,6 +5,7 @@ import android.support.test.espresso.assertion.ViewAssertions
 import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
 import android.support.test.espresso.matcher.ViewMatchers.withId
 import android.support.test.rule.ActivityTestRule
+import android.support.test.rule.GrantPermissionRule
 import android.support.test.runner.AndroidJUnit4
 import id.yanuar.weather.R
 import org.junit.Rule
@@ -24,8 +25,18 @@ class WeatherActivityTest {
     @JvmField
     var activityRule = ActivityTestRule(WeatherActivity::class.java, false, false)
 
+    @Rule
+    @JvmField
+    var grantPermissionRule = GrantPermissionRule.grant(android.Manifest.permission.ACCESS_FINE_LOCATION)
+
     @Test
     fun testStartActivity() {
+        /*if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            getInstrumentation().uiAutomation.executeShellCommand(
+                    "pm grant " + getTargetContext().packageName
+                            + " android.permission.ACCESS_FINE_LOCATION")
+        }*/
+
         activityRule.launchActivity(null)
         onView(withId(R.id.pbLayout)).check(ViewAssertions.matches(isDisplayed()))
     }
