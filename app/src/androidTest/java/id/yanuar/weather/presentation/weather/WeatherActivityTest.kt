bebf7539c84a5ce9c063d7a4faa5a78e8f9c8c73
Yanuar Arifin <halo@yanuar.id>
2018-09-04 02:43:36 +0700

    remove dagger mock, add fake injector creator

--- app/src/androidTest/java/id/yanuar/weather/presentation/weather/WeatherActivityTest.kt
+++ app/src/androidTest/java/id/yanuar/weather/presentation/weather/WeatherActivityTest.kt
@@ -1,5 +1,6 @@
 package id.yanuar.weather.presentation.weather
 
+import android.support.test.InstrumentationRegistry
 import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.assertion.ViewAssertions
 import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
@@ -8,10 +9,15 @@ import android.support.test.rule.ActivityTestRule
 import android.support.test.rule.GrantPermissionRule
 import android.support.test.runner.AndroidJUnit4
 import id.yanuar.weather.R
+import id.yanuar.weather.WeatherApp
+import id.yanuar.weather.fakeActivityInjector
+import id.yanuar.weather.mock
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.Mockito.verify
 import org.mockito.Mockito.`when` as whenever
+
 
 /**
  * Created by Yanuar Arifin
@@ -21,23 +27,37 @@ import org.mockito.Mockito.`when` as whenever
 @RunWith(AndroidJUnit4::class)
 class WeatherActivityTest {
 
+    val mockPresenter = mock<WeatherPresenter>()
+
     @Rule
     @JvmField
-    var activityRule = ActivityTestRule(WeatherActivity::class.java, false, false)
+    var activityRule = object : ActivityTestRule<WeatherActivity>(WeatherActivity::class.java, true, true) {
+        override fun beforeActivityLaunched() {
+            super.beforeActivityLaunched()
+            val app = InstrumentationRegistry.getTargetContext().applicationContext as WeatherApp
+            app.activityInjector = fakeActivityInjector<WeatherActivity> {
+                presenter = mockPresenter
+            }
+        }
+    }
 
     @Rule
     @JvmField
     var grantPermissionRule = GrantPermissionRule.grant(android.Manifest.permission.ACCESS_FINE_LOCATION)
 
     @Test
-    fun testStartActivity() {
-        /*if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            getInstrumentation().uiAutomation.executeShellCommand(
-                    "pm grant " + getTargetContext().packageName
-                            + " android.permission.ACCESS_FINE_LOCATION")
-        }*/
-
-        activityRule.launchActivity(null)
+    fun launchActivityThenShowProgressBar() {
         onView(withId(R.id.pbLayout)).check(ViewAssertions.matches(isDisplayed()))
+    }
+
+    @Test
+    fun launchActivityThenAttachViewToPresenter() {
+        verify(mockPresenter).attachView(activityRule.activity)
+    }
+
+    @Test
+    fun finishActivityThenDetachViewFromPresenter() {
+        activityRule.finishActivity()
+        verify(mockPresenter).detachView()
     }
 }
